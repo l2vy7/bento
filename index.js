60f85821972285552f06f45c335d26d24d56cb34
l2vy7 <102484505+l2vy7@users.noreply.github.com>
2022-08-15 22:05:11 -0400

    i
    when u just realize that embedded loops are syntactically correct

--- index.js
+++ index.js
@@ -8,27 +8,9 @@ module.exports.lint = function (code) {
   for (var i = 0; i < tokens.length; i++) {
     if (tokens[i] === '[') {
       var enclosed = false;
-      if (tokens[i + 1] === '[') {
-        full = false;
-        enclosed = false;
-        errors.push(i.toString() + ' (' + tokens[i] + ') ' + ': Embedded loop from start.');
-        break;
-      }
       for (var token of tokens.slice(i + 1, tokens.length)) {
         if (enclosed === null) break;
-        if (token === '[') {
-          full = false;
-          enclosed = false;
-          errors.push(i.toString() + ' (' + tokens[i] + ') ' + ': Unenclosed loop from start.');
-          break;
-        } else if (token === ']') {
-          var ctok = tokens.slice(i + 1, tokens.length);
-          if (ctok[ctok.indexOf(token) + 1] === ']') {
-            full = false;
-            enclosed = false;
-            errors.push((i + ctok.indexOf(token)).toString() + ' (' + token + ') ' + ': Embedded loop from end.');
-            break;
-          }
+        if (token === ']') {
           enclosed = true;
           break;
         }
@@ -38,27 +20,9 @@ module.exports.lint = function (code) {
         break;
     } else if (tokens[i] === ']') {
       var enclosed = false;
-      if (tokens[i - 1] === ']') {
-        full = false;
-        enclosed = false;
-        errors.push(i.toString() + ' (' + tokens[i] + ') ' + ': Embedded loop from end.');
-        break;
-      }
       for (var token of tokens.slice(0, i - 1)) {
         if (!full) break;
-        if (token === ']') {
-          full = false;
-          enclosed = false;
-          errors.push(i.toString() + ' (' + tokens[i] + ') ' + ': Unenclosed loop from end.');
-          break;
-        } else if (token === '[') {
-          var ctok = tokens.slice(0, i - 1);
-          if (ctok[ctok.indexOf(token) - 1] === '[') {
-            full = false;
-            enclosed = false;
-            errors.push((i + ctok.indexOf(token)).toString() + ' (' + token + ') ' + ': Embedded loop from start.');
-            break;
-          }
+        if (token === '[') {
           enclosed = true;
           break;
         }
